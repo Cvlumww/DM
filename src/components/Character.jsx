cb7b16ca8308c6229c4bc2a28973da1d03042603
Cvlumww <calumwardrop2000@gmail.com>
2022-12-07 20:36:51 +0000

    why isnt this working

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Character = () => {
-  return <></>;
+  return (
+    <div className="CharContainer">
+      <div className="item"></div>
+      <div className="item"></div>
+    </div>
+  );
 };
 
 export default Character;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Header = () => {
-  return <div className="Header">Header</div>;
+  return (
+    <div className="Header">
+      <p>IMAGE HERE</p>
+
+      <div className="buttons">
+        <button className="Add-PC">Add Player Character Here</button>
+        <button className="Add-NPC">Add NPC Here</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
