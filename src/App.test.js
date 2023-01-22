699d835e43195bd59e8015533fc6429a47fa2feb
Cvlumww <calumwardrop2000@gmail.com>
2023-01-22 14:57:04 +0000

    removed the header float on mobile

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,17 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+
+import "@testing-library/jest-dom/extend-expect";
+
 import Character from "./components/Character";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 test("Renders header correctly", () => {
   render(<Header />);
-  const HeaderText = screen.getByText("DnD Combat Initiative List Sorter");
 
-  expect(HeaderText).toBeInTheDocument();
+  // Header Loads Correctly
+  const headerText = screen.getByText("DnD Combat Initiative List Sorter");
+  expect(headerText).toBeInTheDocument();
+
+  //   const headerButtons = screen.
 });
