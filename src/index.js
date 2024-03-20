2f3aee783b874b5b6c70fa813a7bfc6e4a62b7f4
Cvlumww <calumwardrop2000@gmail.com>
2024-03-20 18:21:03 +0000

    Adding GA4 Properly

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 ReactGA.initialize("G-M8KGQPEHCS");
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 root.render(
   <React.StrictMode>
