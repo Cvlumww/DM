27dfb8085ee5e25914e02ba56dd7caeb7abe46be
Cvlumww <calumwardrop2000@gmail.com>
2024-03-20 17:56:38 +0000

    Adding GA4 Again

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-// import ReactGA from "react-ga";
-
 import "./css/App.css";
 import { AppContext, AppProvider } from "./AppContext";
-import ReactGA from "react-ga";
 
 import Character from "./components/Character";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
-  ReactGA.initialize("G-M8KGQPEHCS");
-  ReactGA.pageview(window.location.pathname + window.location.search);
-
   return (
     <div className="App">
       <AppProvider>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import ReactGA from "react-ga";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+ReactGA.initialize("G-M8KGQPEHCS");
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 root.render(
   <React.StrictMode>
     <App />
